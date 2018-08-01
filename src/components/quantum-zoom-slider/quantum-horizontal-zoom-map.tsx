69d1c134b7b6c36f51a527142665c9038323b5c8
Xavier Marin <marin.xavier@gmail.com>
2018-08-01 15:54:09 +0200

    drag&drop

--- src/components/quantum-zoom-slider/quantum-horizontal-zoom-map.tsx
+++ src/components/quantum-zoom-slider/quantum-horizontal-zoom-map.tsx
@@ -1,19 +1,25 @@
-import {Component, Prop, Element, EventEmitter, Event, Watch} from '@stencil/core';
-import {GTSLib} from "../../gts.lib";
+import {
+  Component,
+  Prop,
+  Element,
+  EventEmitter,
+  Event,
+  Watch
+} from "@stencil/core";
+import { GTSLib } from "../../gts.lib";
+import Draggabilly from "draggabilly";
 
 @Component({
-  tag: 'quantum-horizontal-zoom-map',
-  styleUrl: 'quantum-horizontal-zoom-map.scss',
+  tag: "quantum-horizontal-zoom-map",
+  styleUrl: "quantum-horizontal-zoom-map.scss",
   shadow: true
 })
-
 export class QuantumHorizontalZoomMap {
-
   @Prop() width: number;
   @Prop() maxValue: number;
   @Prop() minValue: number;
   @Prop() cursorSize: string = "{}";
-  @Prop() config: string = '{}';
+  @Prop() config: string = "{}";
   @Prop() img: string;
 
   @Element() el: HTMLElement;
@@ -21,13 +27,12 @@ export class QuantumHorizontalZoomMap {
   @Event() xSliderValueChanged: EventEmitter;
   @Event() xZoom: EventEmitter;
 
-
   private _config = {
     rail: {
-      class: ''
+      class: ""
     },
     cursor: {
-      class: ''
+      class: ""
     }
   };
   private _rail: HTMLElement;
@@ -39,6 +44,7 @@ export class QuantumHorizontalZoomMap {
   private _railMax;
   private _mouseCursorLeftOffset;
   private _mouseCursorRightOffset;
+  private lastPos = 0;
 
   @Watch("cursorSize")
   changeCursorSize(newValue: string, oldValue: string) {
@@ -46,11 +52,18 @@ export class QuantumHorizontalZoomMap {
       let object = JSON.parse(newValue);
       if (object.cursorOffset + object.cursorSize <= 100) {
         this._cursor.style.left = (object.cursorOffset * 100).toString() + "%";
-        if (object.cursorSize * this._rail.getBoundingClientRect().width < this._cursorMinWidth) {
-          this._cursor.style.width = this._cursorMinWidth.toString() + "px";
-        } else {
-          this._cursor.style.width = (object.cursorSize * 100).toString() + "%";
-        }
+
+        window.requestAnimationFrame(() => {
+          if (
+            object.cursorSize * this._rail.getBoundingClientRect().width <
+            this._cursorMinWidth
+          ) {
+            this._cursor.style.width = this._cursorMinWidth.toString() + "px";
+          } else {
+            this._cursor.style.width =
+              (object.cursorSize * 100).toString() + "%";
+          }
+        });
       }
     }
   }
@@ -72,30 +85,25 @@ export class QuantumHorizontalZoomMap {
     this._rail = this.el.shadowRoot.querySelector("#rail") as HTMLElement;
     this._cursor = this.el.shadowRoot.querySelector("#cursor") as HTMLElement;
     this._img = this.el.shadowRoot.querySelector("#img") as HTMLElement;
+    let drag = new Draggabilly(this._cursor, {
+      axis: "x",
+      containment: this._rail
+    });
+    drag.on("dragStart", (event, pointer) => {
+      this.dimsX(event);
+    });
+    drag.on("dragMove", (event: any, pointer, moveVector) => {
+      let v = event.pageX - this._rail.offsetLeft - this._mouseCursorLeftOffset;
+      v = Math.max(0, v);
+      let value =
+        (v / (this._railMax - this._railMin - this._cursorWidth)) *
+          (this.maxValue - this.minValue) +
+        this.minValue;
+      window.setTimeout(() =>
+        this.xSliderValueChanged.emit({ sliderValue: value })
+      );
+    });
   }
-
-  mouseDown(event) {
-    event.preventDefault();
-    let me = this;
-
-    this.dimsX(event);
-    this._rail.onmousemove = (event) => {this.dragX(event, me)};
-    this._cursor.onmouseup = (event) => {this.stopDrag(me)};
-    this._rail.onmouseup = (event) => {this.stopDrag(me)};
-    this._cursor.onmouseout = (event) => {this.stopDrag(me)};
-    console.log("click", event.pageX - this._rail.offsetLeft);
-  }
-/*
-  dimsX(event) {
-    let railDims = this._rail.getBoundingClientRect() as DOMRect;
-    let cursorDims = this._cursor.getBoundingClientRect() as DOMRect;
-    this._railMin = railDims.x;
-    this._railMax = railDims.width + this._railMin;
-    this._cursorWidth = cursorDims.width;
-    this._mouseCursorLeftOffset = event.x - cursorDims.x;
-    this._mouseCursorRightOffset = cursorDims.width - this._mouseCursorLeftOffset;
-  }
-*/
 
   dimsX(event) {
     let railDims = this._rail.getBoundingClientRect() as DOMRect;
@@ -103,66 +111,56 @@ export class QuantumHorizontalZoomMap {
     this._railMin = this._rail.offsetLeft;
     this._railMax = railDims.width + this._rail.offsetLeft;
     this._cursorWidth = cursorDims.width;
-    this._mouseCursorLeftOffset = event.pageX - this._cursor.offsetLeft - this._rail.offsetLeft;
-    this._mouseCursorRightOffset = cursorDims.width - this._mouseCursorLeftOffset;
-  }
-
-  dragX(event, elem) {
-    event.preventDefault();
-    if ((event.pageX - elem._mouseCursorLeftOffset) >= elem._railMin + 1 && (event.pageX + elem._mouseCursorRightOffset) <= elem._railMax - 1) {
-      let v = event.pageX - elem._rail.offsetLeft - elem._mouseCursorLeftOffset;
-      v = v < 0 ? 0 : v;
-      elem._cursor.style.left = v + "px";
-
-      let value = ((v) / ((this._railMax - this._railMin) - this._cursorWidth)) * (this.maxValue - this.minValue) + this.minValue;
-      this.xSliderValueChanged.emit({sliderValue: value});
-    }
-  }
-
-  stopDrag(elem) {
-    elem._rail.onmouseup = null;
-    elem._rail.onmousemove = null;
-    elem._cursor.onmouseup = null;
-    elem._rail.onmouseout = null;
+    this._mouseCursorLeftOffset =
+      event.pageX - this._cursor.offsetLeft - this._rail.offsetLeft;
+    this._mouseCursorRightOffset =
+      cursorDims.width - this._mouseCursorLeftOffset;
   }
 
   xWheel(event) {
     event.preventDefault();
     let railDims = this._rail.getBoundingClientRect() as DOMRect;
-
     let coef = (event.pageX - this._rail.offsetLeft) / railDims.width;
-    this.xZoom.emit({zoomValue: {coef: coef, zoomType: event.deltaY * -1}});
+    this.xZoom.emit({ zoomValue: { coef: coef, zoomType: event.deltaY * -1 } });
   }
 
-  positionClick(event){
+  positionClick(event) {
     event.preventDefault();
-    
-    if(event.pageX < this._railMin + this._cursor.offsetLeft || event.pageX > this._railMin + this._cursor.offsetLeft + this._cursorWidth){
+    if (
+      event.pageX < this._railMin + this._cursor.offsetLeft ||
+      event.pageX > this._railMin + this._cursor.offsetLeft + this._cursorWidth
+    ) {
       this.dimsX(event);
-      let halfCursorWidth = this._cursorWidth / 2
+      let halfCursorWidth = this._cursorWidth / 2;
       let v;
-      if(event.pageX - halfCursorWidth < this._rail.offsetLeft){
+      if (event.pageX - halfCursorWidth < this._rail.offsetLeft) {
         v = 0;
         this._cursor.style.left = "1px";
-      }else if(event.pageX + halfCursorWidth > this._railMax){
+      } else if (event.pageX + halfCursorWidth > this._railMax) {
         v = this._railMax - this._railMin - this._cursorWidth;
         this._cursor.style.left = v.toString() + "px";
-      }else{
+      } else {
         v = event.pageX - this._railMin - halfCursorWidth;
         //v = event.pageX - this._rail.offsetLeft - halfCursorWidth;
         this._cursor.style.left = v.toString() + "px";
       }
-     
-      let value = ((v) / ((this._railMax - this._railMin) - this._cursorWidth)) * (this.maxValue - this.minValue) + this.minValue;
-      this.xSliderValueChanged.emit({sliderValue: value});
+
+      let value =
+        (v / (this._railMax - this._railMin - this._cursorWidth)) *
+          (this.maxValue - this.minValue) +
+        this.minValue;
+      this.xSliderValueChanged.emit({ sliderValue: value });
     }
-    
   }
 
   render() {
     return (
-      <div id="rail" onWheel={(event) => this.xWheel(event)} onMouseUp={(event => this.positionClick(event))}>
-        <div id="cursor" onMouseDown={(event) => this.mouseDown(event)} />
+      <div
+        id="rail"
+        onWheel={event => this.xWheel(event)}
+        onMouseUp={event => this.positionClick(event)}
+      >
+        <div id="cursor" />
         <img id="img" src={this.img} />
       </div>
     );
