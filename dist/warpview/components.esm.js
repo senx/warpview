2a149d306939e3340cd4652c017d93f742947686
Xavier Marin <marin.xavier@gmail.com>
2018-11-21 09:11:33 +0100

    build

--- dist/warpview/components.esm.js
+++ dist/warpview/components.esm.js
@@ -57,7 +57,7 @@ var WarpViewChipComponent = /** @class **/ (function() {
   WarpViewChip.is = 'warp-view-chip';
   WarpViewChip.getModule = function(opts) {
     
-    return import('./1otltelr.js').then(function(m) {
+    return import('./49dzxkbe.js').then(function(m) {
         return m.WarpViewChip;
       });
 
@@ -83,7 +83,7 @@ var WarpViewGtsTreeComponent = /** @class **/ (function() {
   WarpViewGtsTree.is = 'warp-view-gts-tree';
   WarpViewGtsTree.getModule = function(opts) {
     
-    return import('./1otltelr.js').then(function(m) {
+    return import('./49dzxkbe.js').then(function(m) {
         return m.WarpViewGtsTree;
       });
 
@@ -239,7 +239,7 @@ var WarpViewTreeViewComponent = /** @class **/ (function() {
   WarpViewTreeView.is = 'warp-view-tree-view';
   WarpViewTreeView.getModule = function(opts) {
     
-    return import('./1otltelr.js').then(function(m) {
+    return import('./49dzxkbe.js').then(function(m) {
         return m.WarpViewTreeView;
       });
 
