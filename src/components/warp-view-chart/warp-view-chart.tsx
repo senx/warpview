98d247aab529c7a85850433689b6ce8561ddc8da
Xavier Marin <marin.xavier@gmail.com>
2018-11-26 11:49:27 +0100

    handling mouseOut on graph

--- src/components/warp-view-chart/warp-view-chart.tsx
+++ src/components/warp-view-chart/warp-view-chart.tsx
@@ -68,10 +68,8 @@ export class WarpViewChart {
   private uuid = 'chart-' + ChartLib.guid().split('-').join('');
   private ticks = [];
   private visibility: boolean[] = [];
-  private showInRangeSelector: boolean[] = [];
   private initialHeight: number;
   private parentWidth = -1;
-  private lastClickedGraph;
 
   @Listen('window:resize')
   onResize() {
@@ -121,10 +119,9 @@ export class WarpViewChart {
   private handleMouseOut(evt: MouseEvent) {
     this.LOG.debug(['handleMouseOut'], evt);
     const legend = this.el.querySelector('.dygraph-legend') as HTMLElement;
-    this.LOG.debug(['handleMouseOut'], [evt, legend]);
     window.setTimeout(() => {
       legend.style.display = 'none';
-    }, 5000)
+    }, 1000)
   }
 
   private gtsToData(gtsList) {
@@ -158,7 +155,6 @@ export class WarpViewChart {
           let color = ColorLib.getColor(pos);
           labels[i + 1] = label;
           colors[i] = color;
-          this.showInRangeSelector.push(true);
           this.visibility.push(this.hiddenData.filter((h) => h === label).length === 0);
         }
         pos++;
@@ -244,87 +240,88 @@ export class WarpViewChart {
     });
   }
 
-  private click(event, g, context) {
-    this.LOG.debug(['click'], g);
-    this.lastClickedGraph = g;
-    event.preventDefault();
-  }
-
-  private scroll(event, g, context) {
+  private scroll(event, g) {
     if(!event.altKey) return;
     this.LOG.debug(['scroll'], g);
-    if (this.lastClickedGraph != g) {
-      return;
-    }
     const normal = event.detail ? event.detail * -1 : event.wheelDelta / 40;
     // For me the normalized value shows 0.075 for one click. If I took
     // that verbatim, it would be a 7.5%.
     const percentage = normal / 50;
-
     if (!(event.offsetX && event.offsetY)) {
       event.offsetX = event.layerX - event.target.offsetLeft;
       event.offsetY = event.layerY - event.target.offsetTop;
     }
 
-    const percentages = this.offsetToPercentage(g, event.offsetX, event.offsetY);
+    const percentages = WarpViewChart.offsetToPercentage(g, event.offsetX, event.offsetY);
     const xPct = percentages[0];
     const yPct = percentages[1];
 
-    this.zoom(g, percentage, xPct, yPct);
+    WarpViewChart.zoom(g, percentage, xPct, yPct);
     event.preventDefault();
   }
 
-  private offsetToPercentage(g, offsetX, offsetY) {
+  private static offsetToPercentage(g, offsetX, offsetY) {
     // This is calculating the pixel offset of the leftmost date.
     const xOffset = g.toDomCoords(g.xAxisRange()[0], null)[0];
     const yar0 = g.yAxisRange(0);
-
     // This is calculating the pixel of the higest value. (Top pixel)
     const yOffset = g.toDomCoords(null, yar0[1])[1];
-
     // x y w and h are relative to the corner of the drawing area,
     // so that the upper corner of the drawing area is (0, 0).
     const x = offsetX - xOffset;
     const y = offsetY - yOffset;
-
     // This is computing the rightmost pixel, effectively defining the
     // width.
     const w = g.toDomCoords(g.xAxisRange()[1], null)[0] - xOffset;
-
     // This is computing the lowest pixel, effectively defining the height.
     const h = g.toDomCoords(null, yar0[0])[1] - yOffset;
-
     // Percentage from the left.
     const xPct = w === 0 ? 0 : (x / w);
     // Percentage from the top.
     const yPct = h === 0 ? 0 : (y / h);
-
     // The (1-) part below changes it from "% distance down from the top"
     // to "% distance up from the bottom".
     return [xPct, (1 - yPct)];
   }
 
-  private zoom(g, zoomInPercentage, xBias, yBias) {
+  private static adjustAxis(axis, zoomInPercentage, bias) {
+    const delta = axis[1] - axis[0];
+    const increment = delta * zoomInPercentage;
+    const foo = [increment * bias, increment * (1 - bias)];
+    return [axis[0] + foo[0], axis[1] - foo[1]];
+  }
+
+  private static zoom(g, zoomInPercentage, xBias, yBias) {
     xBias = xBias || 0.5;
     yBias = yBias || 0.5;
-
-    function adjustAxis(axis, zoomInPercentage, bias) {
-      const delta = axis[1] - axis[0];
-      const increment = delta * zoomInPercentage;
-      const foo = [increment * bias, increment * (1 - bias)];
-      return [axis[0] + foo[0], axis[1] - foo[1]];
-    }
-
     const yAxes = g.yAxisRanges();
     const newYAxes = [];
     for (let i = 0; i < yAxes.length; i++) {
-      newYAxes[i] = adjustAxis(yAxes[i], zoomInPercentage, yBias);
+      newYAxes[i] = WarpViewChart.adjustAxis(yAxes[i], zoomInPercentage, yBias);
     }
-
     g.updateOptions({
-      dateWindow: adjustAxis(g.xAxisRange(), zoomInPercentage, xBias),
+      dateWindow: WarpViewChart.adjustAxis(g.xAxisRange(), zoomInPercentage, xBias),
       valueRange: newYAxes[0]
     });
+  }
+
+  private drawCallback(dygraph, is_initial) {
+    this.LOG.debug(['drawCallback'], [dygraph.dateWindow_, is_initial]);
+    if (dygraph.dateWindow_) {
+      this.boundsDidChange.emit({
+        bounds: {
+          min: dygraph.dateWindow_[0],
+          max: dygraph.dateWindow_[1]
+        }
+      });
+    } else {
+      this.boundsDidChange.emit({
+        bounds: {
+          min: Math.min.apply(null, this.ticks),
+          max: Math.max.apply(null, this.ticks)
+        }
+      });
+    }
   }
 
   private drawChart() {
@@ -341,9 +338,7 @@ export class WarpViewChart {
       const color = this._options.gridLineColor;
       let interactionModel = Dygraph.defaultInteractionModel;
       interactionModel.mousewheel = this.scroll.bind(this);
-      interactionModel.click = this.click.bind(this);
-
-      this.LOG.debug(['drawChart', 'Dygraph.defaultInteractionModel'], Dygraph.defaultInteractionModel);
+      interactionModel.mouseout = this.handleMouseOut.bind(this);
       let options: Options = {
         height: this.displayGraph() ? (this.responsive ? this.el.parentElement.clientHeight : WarpViewChart.DEFAULT_HEIGHT) - 30 : 30,
         width: (this.responsive ? this.el.parentElement.clientWidth : WarpViewChart.DEFAULT_WIDTH) - 5,
@@ -376,31 +371,14 @@ export class WarpViewChart {
             drawAxis: this.displayGraph()
           },
           y: {
-            axisLabelFormatter: (x, granularity, opts, dygraph) => {
+            axisLabelFormatter: (x) => {
               return WarpViewChart.toFixed(x);
             }
           }
         },
         legendFormatter: this.legendFormatter,
         highlightCallback: this.highlightCallback.bind(this),
-        drawCallback: ((dygraph, is_initial) => {
-          this.LOG.debug(['drawCallback'], [dygraph.dateWindow_, is_initial]);
-          if (dygraph.dateWindow_) {
-            this.boundsDidChange.emit({
-              bounds: {
-                min: dygraph.dateWindow_[0],
-                max: dygraph.dateWindow_[1]
-              }
-            });
-          } else {
-            this.boundsDidChange.emit({
-              bounds: {
-                min: Math.min.apply(null, this.ticks),
-                max: Math.max.apply(null, this.ticks)
-              }
-            });
-          }
-        }).bind(this),
+        drawCallback: this.drawCallback.bind(this),
         axisLabelWidth: this.standalone ? 50 : 94,
         rightGap: this.standalone ? 0 : 20,
         interactionModel: interactionModel
@@ -411,7 +389,7 @@ export class WarpViewChart {
         chart.style.height = '30px';
       }
       if (this._options.timeMode === 'timestamp') {
-        options.axes.x.axisLabelFormatter = (x, granularity, opts, dygraph) => {
+        options.axes.x.axisLabelFormatter = (x) => {
           return WarpViewChart.toFixed(x);
         }
       }
@@ -421,7 +399,7 @@ export class WarpViewChart {
         options
       );
 
-      this.LOG.debug(['options.height'], options.height)
+      this.LOG.debug(['options.height'], options.height);
       this.onResize();
     }
   }
@@ -441,7 +419,6 @@ export class WarpViewChart {
   render() {
     return <div>
       <div id={this.uuid} class="chart"/>
-      {/* <div class="tooltip" id={this.uuid + '-tooltip'}></div>*/}
     </div>;
   }
 }
