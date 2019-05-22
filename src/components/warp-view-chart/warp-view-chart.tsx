ee161894c9f6b3612af0f06022e54ce6d8ef45fe
pi-r-p <pierre.papin@senx.io>
2019-05-22 11:55:56 +0200

    regressions fix

--- src/components/warp-view-chart/warp-view-chart.tsx
+++ src/components/warp-view-chart/warp-view-chart.tsx
@@ -381,7 +381,7 @@ export class WarpViewChart {
     //build the big matrix for dygraph from the data hashSet.
     const divider = GTSLib.getDivider(this._options.timeUnit);
     this.LOG.debug(['chart', 'divider', 'timeunit'], divider, this._options.timeUnit);
-    Object.keys(this.dataHashset).forEach(timestamp => {
+    Object.keys(this.dataHashset).some(timestamp => {
       if (this._options.timeMode && this._options.timeMode === 'timestamp') {
         this.dygraphdataSets.push([parseInt(timestamp)].concat(this.dataHashset[timestamp]));
       } else {
