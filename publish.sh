2b6ed9464e3b8b5c9355f51ea3b55454e008ef4b
xavier <marin.xavier@gmail.com>
2021-02-02 15:05:44 +0100

    add changelog

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 yarn version --new-version $1
-yarn cl
 (cd projects/warpview-ng && yarn version --new-version $1 )
 yarn clean
 yarn build
 (cd dist/warpview && yarn publish --new-version $1 )
+yarn cl
